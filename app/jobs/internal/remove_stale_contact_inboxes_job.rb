f87f42a50617feac6f5bc2af9261725d0dd43870
nestordavalos <mestordavalos@gmail.com>
2024-12-31 03:10:33 +0000

    fix: RemoveStaleRedisKeys service

--- app/jobs/internal/remove_stale_contact_inboxes_job.rb
+++ app/jobs/internal/remove_stale_contact_inboxes_job.rb
@@ -2,10 +2,11 @@
 # remove contact inboxes that does not have any conversations
 # and are older than 3 months
 
-class Internal::RemoveStaleContactInboxesJob < ApplicationJob
-  queue_as :scheduled_jobs
+class Internal::ProcessStaleRedisKeysJob < ApplicationJob
+  queue_as :low
 
-  def perform
-    Internal::RemoveStaleContactInboxesService.new.perform
+  def perform(account)
+    removed_count = Internal::RemoveStaleRedisKeysService.new(account_id: account.id).perform
+    Rails.logger.info "Successfully cleaned up Redis keys for account #{account.id} (removed #{removed_count} keys)"
   end
 end
